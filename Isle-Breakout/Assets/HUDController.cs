9830e65ed23e8b6dbff43c44be8035c5ea97a1c4
Vilius <37084800+Villlius@users.noreply.github.com>
2020-02-23 22:49:07 +0200

    Inventory hotbar UI

---
+++ Isle-Breakout/Assets/HUDController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+
+public class HUDController : MonoBehaviour
+{
+    public GameObject messagePanel;
+
+    public void OpenMessagePanel(string text)
+    {
+        Debug.Log(messagePanel.name);
+        //messagePanel.SetActive(true);
+        //TODO: change messagePanelText
+    }
+
+    public void CloseMessagePanel()
+    {
+        messagePanel.SetActive(false);
+    }
+}
---
+++ Isle-Breakout/Assets/PlayerCollisionController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+
+public class PlayerCollisionController : MonoBehaviourPun
+{
+    public GameObject canvas;
+
+    private CanvasController script;
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "item")
+        {
+            script.ToggleMessagePanel();
+            Debug.Log("Triggered OnCollisionEnter");
+        }
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        if (collision.gameObject.tag == "item")
+        {
+            script.ToggleMessagePanel();
+            Debug.Log("Triggered OnCollisionExit");
+        }
+    }
+
+    private void Start()
+    {
+        canvas = GameObject.Find("PlayerCanvas");
+        //script = canvas.GetComponent(typeof(CanvasController)) as CanvasController;
+        script = canvas.GetComponent<CanvasController>();
+    }
+}
