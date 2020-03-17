7657f9fb387ef29aec79a2124fbc346ad3896c8b
kestv <kestutis.ramulionis@gmail.com>
2020-03-17 23:25:37 +0200

    files only

---
+++ Isle-Breakout/Assets/ErbGameArt/AAA Projectiles/Scripts/ProjectileMover.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProjectileMover : MonoBehaviour
+{
+    public float speed = 15f;
+    public float hitOffset = 0f;
+    public bool UseFirePointRotation;
+    public GameObject hit;
+    public GameObject flash;
+    public GameObject target;
+    private float startTime;
+           
+
+void Start ()
+    {
+        startTime = Time.time;
+        if (flash != null)
+        {
+            var flashInstance = Instantiate(flash, transform.position, Quaternion.identity);
+            flashInstance.transform.forward = gameObject.transform.forward;
+            var flashPs = flashInstance.GetComponent<ParticleSystem>();
+            if (flashPs == null)
+            {
+                Destroy(flashInstance, flashPs.main.duration);
+            }
+            else
+            {
+                var flashPsParts = flashInstance.transform.GetChild(0).GetComponent<ParticleSystem>();
+                Destroy(flashInstance, flashPsParts.main.duration);
+            }
+        }
+	}
+
+    void FixedUpdate ()
+    {
+		if (speed != 0)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, target.transform.position, 10f * Time.deltaTime);
+        }
+        if (Time.time - startTime > 5)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    //https ://docs.unity3d.com/ScriptReference/Rigidbody.OnCollisionEnter.html
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Enemy")
+        {
+            speed = 0;
+            ContactPoint contact = collision.contacts[0];
+            Quaternion rot = Quaternion.FromToRotation(Vector3.up, contact.normal);
+            Vector3 pos = contact.point + contact.normal * hitOffset;
+
+            if (hit != null)
+            {
+                var hitInstance = Instantiate(hit, pos, rot);
+                if (UseFirePointRotation)
+                { hitInstance.transform.rotation = gameObject.transform.rotation * Quaternion.Euler(0, 180f, 0); }
+                else
+                { hitInstance.transform.LookAt(contact.point + contact.normal); }
+
+                var hitPs = hitInstance.GetComponent<ParticleSystem>();
+                if (hitPs == null)
+                {
+                    Destroy(hitInstance, hitPs.main.duration);
+                }
+                else
+                {
+                    var hitPsParts = hitInstance.transform.GetChild(0).GetComponent<ParticleSystem>();
+                    Destroy(hitInstance, hitPsParts.main.duration);
+                }
+            }
+            Destroy(gameObject);
+        }
+    }
+}
