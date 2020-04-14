9f9f1a17a6ca6d3270e423ce41770673755025b5
kestv <kestutis.ramulionis@gmail.com>
2020-04-15 02:25:59 +0300

    chest script

--- Isle-Breakout/Assets/Scripts/Interactable Objects/ChestSettings.cs
+++ Isle-Breakout/Assets/Scripts/Interactable Objects/ChestSettings.cs
@@ -1,17 +1,39 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ChestSettings : MonoBehaviour
 {
+    public GameObject chestPanelPrefab;
     public GameObject chestPanel;
     public int chestSize;
 
+    public List<GameObject> items;
+
     private void Start()
     {
-        chestPanel = Instantiate(chestPanel);
+        chestPanel = Instantiate(chestPanelPrefab);
         chestPanel.GetComponent<ChestPanelController>().InitializeChest(chestSize);
         chestPanel.SetActive(false);
+
+        AddItemToChest(items); ;
+    }
+
+    public void AddItemToChest(List<GameObject> items)
+    {
+        for (int i = 0; i < items.Count; i++)
+        {
+            if (i < chestSize)
+            {
+                Transform slotImage = chestPanel.transform.GetChild(i).GetChild(0);
+                ItemSettings itemSettings = items[i].GetComponent<ItemSettings>();
+
+                Instantiate(items[i], slotImage);
+                slotImage.GetComponent<Image>().sprite = itemSettings.getSprite();
+                slotImage.gameObject.SetActive(true);
+            }
+        }
     }
 
     public GameObject getChestPanel()
