847a9e6929f0ff655f624236a0061652662fc26d
kestv <kestutis.ramulionis@gmail.com>
2020-06-10 20:04:51 +0300

    clouds and scene changes

---
+++ Isle-Breakout/Assets/CloudMovement.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CloudMovement : MonoBehaviour
+{
+    float moveSpeed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        moveSpeed = Random.Range(0.02f, 0.04f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = new Vector3(transform.position.x - moveSpeed, transform.position.y, transform.position.z);
+    }
+}
